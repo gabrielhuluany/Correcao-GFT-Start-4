c33d60e7a64b0fc2cf61c39485986f3f60bb45ce
gabrielhuluany <gabrielhuluany@outlook.com>
2022-03-22 22:24:36 -0300

    Ex1

--- Ex1/Ex1.cs
+++ Ex1/Ex1.cs
@@ -6,6 +6,7 @@ class Program {
   public static void Main (string[] args) {
     bool sucesso = false;
     List<int> lista = new List<int>();
+    List<int> impares = new List<int>();
 
     while(!sucesso) {
       Console.Write("Digite o valor inicial e o valor final do intervalo: ");
@@ -22,16 +23,18 @@ class Program {
       }
 
       if(lista[0] >= 0 && lista[0] <= lista[1]) {
-        Console.WriteLine("[" + lista[0] + "," + lista[1] + "]\n");
+        //Console.WriteLine("[" + lista[0] + "," + lista[1] + "]\n");
         sucesso = true;
       }
     }
 
     for(int i = 0; i <= (lista[1] - lista[0]); i++) {
       if((lista[0] + i) % 2 != 0) {
-        Console.WriteLine((lista[0] + i));
+        impares.Add((lista[0] + i));
       }
     }
+
+    Console.WriteLine("{0}", string.Join(" ", impares));
     Console.WriteLine("\n");
 
     Console.WriteLine("Pressione qualquer tecla para fechar esta janela...");
