3cd5b5824e2e258e00c457716ab2922388b438ab
gabrielhuluany <gabrielhuluany@outlook.com>
2022-03-22 21:32:09 -0300

    Ex2

---
+++ Ex2/Ex2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Pessoa p = new Pessoa();
+
+    p.setNome("Gabriel");
+    p.setEndereco("Rua GFT, 404");
+    p.setTelefone("(00) 90000-0000");
+    Console.WriteLine("Nome: " + p.getNome());
+    Console.WriteLine("Endereço: " + p.getEndereco());
+    Console.WriteLine("Telefone: " + p.getTelefone() + "\n");
+
+    Console.WriteLine("Pressione qualquer tecla para fechar esta janela...");
+    Console.ReadKey();
+  }
+}
